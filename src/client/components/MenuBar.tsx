baace1c407b899d2894e706868754f8d96bfcc2e
Paul Chavard <pro@paul.chavard.net>
2023-10-24 10:25:21 +0200

    improuve editor icons

--- src/client/components/MenuBar.tsx
+++ src/client/components/MenuBar.tsx
@@ -1,4 +1,198 @@
-import { useCurrentEditor } from '@tiptap/react';
+import { ReactNode } from 'react';
+import { type Editor, useCurrentEditor } from '@tiptap/react';
+import {
+  RiBold,
+  RiItalic,
+  RiStrikethrough,
+  RiUnderline,
+  RiCodeView,
+  RiFontColor,
+  RiListOrdered,
+  RiListUnordered,
+  RiAlignLeft,
+  RiAlignRight,
+  RiAlignCenter,
+  RiAlignJustify,
+  RiArrowGoBackLine,
+  RiArrowGoForwardLine,
+} from 'react-icons/ri';
+
+type Action = {
+  icon: ReactNode;
+  label: string;
+  run(): void;
+  isActive(): boolean;
+  isDisabled(): boolean;
+};
+
+function buildActions(editor: Editor): Action[][] {
+  return [
+    [
+      {
+        icon: <RiBold />,
+        label: 'bold',
+        run: () => editor.chain().focus().toggleBold().run(),
+        isActive: () => editor.isActive('bold'),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiItalic />,
+        label: 'italic',
+        run: () => editor.chain().focus().toggleItalic().run(),
+        isActive: () => editor.isActive('italic'),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiCodeView />,
+        label: 'code',
+        run: () => editor.chain().focus().toggleCode().run(),
+        isActive: () => editor.isActive('code'),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiUnderline />,
+        label: 'underline',
+        run: () => editor.chain().focus().toggleUnderline().run(),
+        isActive: () => editor.isActive('underline'),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiStrikethrough />,
+        label: 'strike',
+        run: () => editor.chain().focus().toggleStrike().run(),
+        isActive: () => editor.isActive('strike'),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiFontColor />,
+        label: 'highlight',
+        run: () => editor.chain().focus().toggleHighlight().run(),
+        isActive: () => editor.isActive('highlight'),
+        isDisabled: () => false,
+      },
+    ],
+    [
+      {
+        icon: <RiListUnordered />,
+        label: 'bullet list',
+        run: () => editor.chain().focus().toggleBulletList().run(),
+        isActive: () => editor.isActive('bulletList'),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiListOrdered />,
+        label: 'ordered list',
+        run: () => editor.chain().focus().toggleOrderedList().run(),
+        isActive: () => editor.isActive('orderedList'),
+        isDisabled: () => false,
+      },
+    ],
+    [
+      {
+        icon: <RiAlignLeft />,
+        label: 'left',
+        run: () => editor.chain().focus().setTextAlign('left').run(),
+        isActive: () => editor.isActive({ textAlign: 'left' }),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiAlignCenter />,
+        label: 'center',
+        run: () => editor.chain().focus().setTextAlign('center').run(),
+        isActive: () => editor.isActive({ textAlign: 'center' }),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiAlignRight />,
+        label: 'right',
+        run: () => editor.chain().focus().setTextAlign('right').run(),
+        isActive: () => editor.isActive({ textAlign: 'right' }),
+        isDisabled: () => false,
+      },
+      {
+        icon: <RiAlignJustify />,
+        label: 'justify',
+        run: () => editor.chain().focus().setTextAlign('justify').run(),
+        isActive: () => editor.isActive({ textAlign: 'justify' }),
+        isDisabled: () => false,
+      },
+    ],
+    [
+      {
+        icon: <RiArrowGoBackLine />,
+        label: 'undo',
+        run: () => editor.chain().focus().undo().run(),
+        isActive: () => false,
+        isDisabled: () => !editor.can().chain().focus().undo().run(),
+      },
+      {
+        icon: <RiArrowGoForwardLine />,
+        label: 'redo',
+        run: () => editor.chain().focus().redo().run(),
+        isActive: () => false,
+        isDisabled: () => !editor.can().chain().focus().redo().run(),
+      },
+    ],
+  ];
+}
+
+function StartButton({ action }: { action: Action }) {
+  return (
+    <button
+      type="button"
+      onClick={() => action.run()}
+      disabled={action.isDisabled()}
+      className={`relative inline-flex items-center rounded-l-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${dynamicStyles(
+        action
+      )}`}
+      title={action.label}
+    >
+      {action.icon}
+    </button>
+  );
+}
+
+function EndButton({ action }: { action: Action }) {
+  return (
+    <button
+      type="button"
+      onClick={() => action.run()}
+      disabled={action.isDisabled()}
+      className={`relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${dynamicStyles(
+        action
+      )}`}
+      title={action.label}
+    >
+      {action.icon}
+    </button>
+  );
+}
+
+function CenterButton({ action }: { action: Action }) {
+  return (
+    <button
+      type="button"
+      onClick={() => action.run()}
+      disabled={action.isDisabled()}
+      className={`relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${dynamicStyles(
+        action
+      )}`}
+      title={action.label}
+    >
+      {action.icon}
+    </button>
+  );
+}
+
+function dynamicStyles(action: Action) {
+  return `${action.isActive() ? 'bg-blue-200' : 'bg-white'} ${
+    action.isDisabled()
+      ? 'opacity-50'
+      : action.isActive()
+      ? 'hover:bg-blue-50'
+      : 'hover:bg-gray-50'
+  }`;
+}
 
 export function MenuBar() {
   const { editor } = useCurrentEditor();
@@ -7,167 +201,24 @@ export function MenuBar() {
     return null;
   }
 
+  const actions = buildActions(editor);
+
   return (
     <div className="flex gap-2 m-2 mb-0">
-      <div className="isolate inline-flex rounded-md shadow-sm">
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={`relative inline-flex items-center rounded-l-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive('bold')
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          bold
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={`relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive('italic')
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          italic
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleCode().run()}
-          className={`relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive('code')
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          code
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleUnderline().run()}
-          className={`relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive('underline')
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          underline
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={`relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive('strike')
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          strike
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleHighlight().run()}
-          className={`relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive('highlight')
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          highlight
-        </button>
-      </div>
-
-      <div>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={`relative inline-flex items-center rounded-l-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive('bulletList')
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          bullet list
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={`relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive('orderedList')
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          ordered list
-        </button>
-      </div>
-
-      <div>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().setTextAlign('left').run()}
-          className={`relative inline-flex items-center rounded-l-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive({ textAlign: 'left' })
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          left
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().setTextAlign('center').run()}
-          className={`relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive({ textAlign: 'center' })
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          center
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().setTextAlign('right').run()}
-          className={`relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive({ textAlign: 'right' })
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          right
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().setTextAlign('justify').run()}
-          className={`relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 ${
-            editor.isActive({ textAlign: 'justify' })
-              ? 'bg-blue-200 hover:bg-blue-50'
-              : 'bg-white hover:bg-gray-50'
-          }`}
-        >
-          justify
-        </button>
-      </div>
-
-      <div>
-        <button
-          type="button"
-          className="relative inline-flex items-center rounded-l-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-10 bg-white hover:bg-gray-50"
-          onClick={() => editor.chain().focus().undo().run()}
-          disabled={!editor.can().chain().focus().undo().run()}
-        >
-          undo
-        </button>
-        <button
-          type="button"
-          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-          onClick={() => editor.chain().focus().redo().run()}
-          disabled={!editor.can().chain().focus().redo().run()}
-        >
-          redo
-        </button>
-      </div>
+      {actions.map((group, index) => {
+        return (
+          <div key={index} className="isolate inline-flex rounded-md shadow-sm">
+            {group.map((action, index) => {
+              if (index == 0) {
+                return <StartButton key={index} action={action} />;
+              } else if (index == group.length - 1) {
+                return <EndButton key={index} action={action} />;
+              }
+              return <CenterButton key={index} action={action} />;
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
